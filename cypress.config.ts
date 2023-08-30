9631cdc6a1d3294eee338043c989477cca663648
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2023-08-30 20:50:02 +0800

    fix: custom install command before running cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,19 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.family === "firefox") {
+          // launchOptions.preferences is a map of preference names to values
+          // crypto-hash is not working in firefox when
+          // testing_localhost_is_secure_when_hijacked is false
+          // https://github.com/cypress-io/cypress/issues/14600
+          launchOptions.preferences[
+            "network.proxy.testing_localhost_is_secure_when_hijacked"
+          ] = true;
+        }
+
+        return launchOptions;
+      });
     },
     baseUrl: "http://localhost:3000",
   },
