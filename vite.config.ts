0d668c2224491a94a6512bfc4c6b0ecac6d0e105
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2024-10-14 12:01:36 +0800

    replace cra with vite

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, loadEnv  } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+});
