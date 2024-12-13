ea340e84fb91d2b8d5380edce2d614e0e95aa267
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2024-12-13 16:52:54 +0800

    create plotly custom bundle from git version
    bump minor deps

--- plotly-custom-build.sh
+++ plotly-custom-build.sh
@@ -2,7 +2,10 @@
 # https://github.com/plotly/plotly.js/blob/master/CUSTOM_BUNDLE.md
 CWD=${PWD}
 npm i
-cd node_modules/plotly.js
+git clone --branch v2.35.2 --depth 1 https://github.com/plotly/plotly.js.git
+cd plotly.js
 npm i
-npm run custom-bundle -- --out suv --traces scatter,surface,contour --transforms none
+npm run custom-bundle -- --out suv --traces scatter,surface,contour
+cp dist/plotly-suv.min.js ../node_modules/plotly.js/dist/plotly-suv.min.js
 cd ${CWD}
+rm -rf plotly.js
