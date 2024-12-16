ddf8d9c95aefa1f66c7da6ac5fa3135d06fd309f
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2024-12-16 17:50:25 +0800

    bump plotly, undo transforms flag in plotly build script

--- plotly-custom-build.sh
+++ plotly-custom-build.sh
@@ -2,10 +2,10 @@
 # https://github.com/plotly/plotly.js/blob/master/CUSTOM_BUNDLE.md
 CWD=${PWD}
 npm i
-git clone --branch v2.35.2 --depth 1 https://github.com/plotly/plotly.js.git
+git clone --branch v2.35.3 --depth 1 https://github.com/plotly/plotly.js.git
 cd plotly.js
 npm i
-npm run custom-bundle -- --out suv --traces scatter,surface,contour
+npm run custom-bundle -- --out suv --traces scatter,surface,contour --transforms none
 cp dist/plotly-suv.min.js ../node_modules/plotly.js/dist/plotly-suv.min.js
 cd ${CWD}
 rm -rf plotly.js
