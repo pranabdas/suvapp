7adce80e147bea5226dd0d40b1da4540493248b0
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-01-06 10:31:04 +0800

    try prefetching lazy components + add netlify headers for better cache control

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,10 @@ import RenderTable from "./RenderTable";
 import Footer from "./Footer";
 import ConsoleTests from "./ConsoleTests";
 
-const PlotComponent = lazy(() => import("./PlotComponent"));
-const Plot3dSurface = lazy(() => import("./Plot3dSurface"));
+const PlotComponentPromise = import("./PlotComponent");
+const PlotComponent = lazy(() => PlotComponentPromise);
+const Plot3dSurfacePromise = import("./Plot3dSurface")
+const Plot3dSurface = lazy(() => Plot3dSurfacePromise);
 
 function App(): JSX.Element {
   const [filename, setFilename] = useState("");
