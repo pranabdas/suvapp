a75e60759d89f9ea5581713ab72f6826235494d6
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-01-06 11:21:24 +0800

    prefetch assets, better cache control, update plotly custom build script, bump deps (#7)
    * create plotly custom bundle from git version
    
    bump minor deps
    
    * upgrade vite to v6
    
    * bump plotly, undo transforms flag in plotly build script
    
    * try prefetching lazy components + add netlify headers for better cache control
    
    * use vite-plugin-bundle-prefetch
    
    * fix: move _headers to the public

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import prefetchPlugin from "vite-plugin-bundle-prefetch";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), prefetchPlugin()],
   server: {
     port: 3000,
   },
