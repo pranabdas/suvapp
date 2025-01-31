b6cc8d84a952704b1ce52b463d56e0d1bf504f70
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-01-31 11:52:47 +0800

    bump plotly to v3

--- plotly-custom-build.sh
+++ plotly-custom-build.sh
@@ -2,10 +2,10 @@
 # https://github.com/plotly/plotly.js/blob/master/CUSTOM_BUNDLE.md
 CWD=${PWD}
 npm i
-git clone --branch v2.35.3 --depth 1 https://github.com/plotly/plotly.js.git
+git clone --branch v3.0.0 --depth 1 https://github.com/plotly/plotly.js.git
 cd plotly.js
 npm i
-npm run custom-bundle -- --out suv --traces scatter,surface,contour --transforms none --strict
+npm run custom-bundle -- --out suv --traces scatter,surface,contour --strict
 cp dist/plotly-suv.min.js ../node_modules/plotly.js/dist/plotly-suv.min.js
 cd ${CWD}
 rm -rf plotly.js
