47ea3c08cf7d0aa85c7d11a18b4517e20459ffe0
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-08-09 00:56:39 +0800

    chore: print plotly version during the build

--- plotly-custom-build.sh
+++ plotly-custom-build.sh
@@ -2,7 +2,9 @@
 # https://github.com/plotly/plotly.js/blob/master/CUSTOM_BUNDLE.md
 CWD=${PWD}
 npm i
-git clone --branch v$( jq -r '.dependencies["plotly.js"]' package.json | sed 's/[^0-9\.]*//g' ) --depth 1 https://github.com/plotly/plotly.js.git
+PLOTLY_VERSION=$( jq -r '.dependencies["plotly.js"]' package.json | sed 's/[^0-9\.]*//g' )
+echo -e "\033[36m>==> Detected plotly version \033[0m\033[1m${PLOTLY_VERSION}\033[0m\033[36m from package.json\033[0m"
+git clone --branch v${PLOTLY_VERSION} --depth 1 https://github.com/plotly/plotly.js.git
 cd plotly.js
 npm i
 npm run custom-bundle -- --out suv --traces scatter,surface,contour --strict
