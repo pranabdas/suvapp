935abe63229e8797093bc62815407106a5278747
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-08-15 11:50:30 +0800

    remove comments and blank lines from js bundle

--- plotly-custom-build.sh
+++ plotly-custom-build.sh
@@ -3,11 +3,13 @@
 CWD=${PWD}
 npm i
 PLOTLY_VERSION=$( jq -r '.dependencies["plotly.js"]' package.json | sed 's/[^0-9\.]*//g' )
-echo -e "\033[36m>==> Detected plotly version \033[0m\033[1m${PLOTLY_VERSION}\033[0m\033[36m from package.json\033[0m"
+echo -e "\033[36m==> Bundling plotly version \033[0m\033[1m${PLOTLY_VERSION}\033[0m"
 git clone --branch v${PLOTLY_VERSION} --depth 1 https://github.com/plotly/plotly.js.git
 cd plotly.js
 npm i
 npm run custom-bundle -- --out suv --traces scatter,surface,contour --strict
+sed -i.bak '/^[[:space:]]*\/\//d' dist/plotly-suv.min.js
+sed -i.bak '/^[[:space:]]*$/d' dist/plotly-suv.min.js
 cp dist/plotly-suv.min.js ../node_modules/plotly.js/dist/plotly-suv.min.js
 cd ${CWD}
 rm -rf plotly.js
