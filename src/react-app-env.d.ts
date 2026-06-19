5ffcdc7aa814557cb701e62474a9a9dd85834c9d
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2026-06-19 12:27:49 +0800

    get rid of react-app-env.d.ts

--- src/react-app-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react-scripts" />
-
-// https://github.com/plotly/react-plotly.js/issues/226
-// https://github.com/plotly/react-plotly.js/issues/80
-declare module "plotly.js/dist/plotly-suv.min.js" {
-    export { Plotly as default } from "plotly.js";
-}
